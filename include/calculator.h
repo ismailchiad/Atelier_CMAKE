7e231eaf1952238cf46c975f26ad98f3086c70bd
ismailchiad <166799810+ismailchiad@users.noreply.github.com>
2024-05-23 11:40:57 +0200

    Update calculator.h

--- include/calculator.h
+++ include/calculator.h
@@ -6,8 +6,4 @@ double _add(double a, double b);
 double _sub(double a, double b);
 double _mul(double a, double b);
 double _div(double a, double b);
-
-// Déclaration de la nouvelle fonction pour calculer le carré
-int car(int n);
-
-#endif // CALCULATOR_H
+double _car(double a);
