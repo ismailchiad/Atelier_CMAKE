f6583d7cd6701982cf4d8153d33131834612b73e
ismailchiad <166799810+ismailchiad@users.noreply.github.com>
2024-05-23 11:49:13 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "./../include/calculator.h"
+#include "calculator.h"
 
 int main(int argc, char *argv[]) {
     if (argc == 4) {
